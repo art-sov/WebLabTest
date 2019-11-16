e048940238bd636df2395f68bd6d5e93328f7341
art-sov <artsovalov@gmail.com>
2019-11-17 01:03:55 +0200

    added fragments in main activity

--- app/src/main/java/com/weblab/MainActivity.java
+++ app/src/main/java/com/weblab/MainActivity.java
@@ -6,9 +6,29 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static PrefConfig prefConfig;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        prefConfig = new PrefConfig(this);
+
+        if(findViewById(R.id.fragment_container) != null)
+        {
+            if (savedInstanceState != null)
+            {
+                return;
+            }
+            if (prefConfig.readLoginStatus())
+            {
+                getSupportFragmentManager().beginTransaction()
+                        .add(R.id.fragment_container, new StatusFragment()).commit();
+            }
+            else
+            {
+                getSupportFragmentManager().beginTransaction()
+                        .add(R.id.fragment_container, new LoginFragment()).commit();
+            }
+        }
     }
 }
