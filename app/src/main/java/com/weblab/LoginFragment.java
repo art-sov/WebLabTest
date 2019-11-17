f4f24c107958f72855faca8bc4299c9dae927379
art-sov <artsovalov@gmail.com>
2019-11-17 15:55:02 +0200

    implement launch registration form

--- app/src/main/java/com/weblab/LoginFragment.java
+++ app/src/main/java/com/weblab/LoginFragment.java
@@ -1,13 +1,17 @@
 package com.weblab;
 
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -15,6 +19,8 @@ import android.view.ViewGroup;
  */
 public class LoginFragment extends Fragment {
 
+    private TextView tvRegistrate;
+    OnLoginFormActivityListener listener;
 
     public LoginFragment() {
         // Required empty public constructor
@@ -24,8 +30,29 @@ public class LoginFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        View view =  inflater.inflate(R.layout.fragment_login, container, false);
+        tvRegistrate = view.findViewById(R.id.tvRegistrate);
+
+        tvRegistrate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listener.performRegister();
+            }
+        });
+        return view;
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        Activity activity = (Activity) context;
+        listener = (OnLoginFormActivityListener) activity;
+    }
+
+    public interface OnLoginFormActivityListener
+    {
+        public void performRegister();
+        public void performLogin(String name);
     }
 
 }
--- app/src/main/java/com/weblab/MainActivity.java
+++ app/src/main/java/com/weblab/MainActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoginFragment.OnLoginFormActivityListener {
 
     private static PrefConfig prefConfig;
     @Override
@@ -30,5 +30,18 @@ public class MainActivity extends AppCompatActivity {
                         .add(R.id.fragment_container, new LoginFragment()).commit();
             }
         }
+    }
+
+    @Override
+    public void performRegister() {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, new RegistrationFragment())
+                .addToBackStack(null)
+                .commit();
+    }
+
+    @Override
+    public void performLogin(String name) {
+
     }
 }
